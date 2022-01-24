d79dc8dc20bdfb1f7ad77edb6c7360c5a5f92fc7
JohnTeoJF <johnteojf+github@gmail.com>
2022-01-24 16:34:56 +0800

    Test #3

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/ibf2021-d21'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+'/dist/ibf2021-d21/index.html'));});
 app.listen(process.env.PORT || 8080);
